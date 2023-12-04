35ef4bf9bbbb246954dde0853cdbc5a71cf57e4b
vicheath <vicheath.a.s@gmail.com>
2023-12-04 13:55:39 -0500

    second day

--- sba.js
+++ sba.js
@@ -1,12 +1,166 @@
 //SBA 308: JavaScript Fundamentals
 
 // The provided course information.
-const CourseInfo = {
+// const CourseInfo = {
+//     id: 451,
+//     name: "Introduction to JavaScript"
+//   };
+  
+//   // The provided assignment group.
+//   const AssignmentGroup = {
+//     id: 12345,
+//     name: "Fundamentals of JavaScript",
+//     course_id: 451,
+//     group_weight: 25,
+//     assignments: [
+//       {
+//         id: 1,
+//         name: "Declare a Variable",
+//         due_at: "2023-01-25",
+//         points_possible: 50
+//       },
+//       {
+//         id: 2,
+//         name: "Write a Function",
+//         due_at: "2023-02-27",
+//         points_possible: 150
+//       },
+//       {
+//         id: 3,
+//         name: "Code the World",
+//         due_at: "3156-11-15",
+//         points_possible: 500
+//       }
+//     ]
+//   };
+  
+//   // The provided learner submission data.
+//   const LearnerSubmissions = [
+//     {
+//       learner_id: 125,
+//       assignment_id: 1,
+//       submission: {
+//         submitted_at: "2023-01-25",
+//         score: 47
+//       }
+//     },
+//     {
+//       learner_id: 125,
+//       assignment_id: 2,
+//       submission: {
+//         submitted_at: "2023-02-12",
+//         score: 150
+//       }
+//     },
+//     {
+//       learner_id: 125,
+//       assignment_id: 3,
+//       submission: {
+//         submitted_at: "2023-01-25",
+//         score: 400
+//       }
+//     },
+//     {
+//       learner_id: 132,
+//       assignment_id: 1,
+//       submission: {
+//         submitted_at: "2023-01-24",
+//         score: 39
+//       }
+//     },
+//     {
+//       learner_id: 132,
+//       assignment_id: 2,
+//       submission: {
+//         submitted_at: "2023-03-07",
+//         score: 140
+//       }
+//     }
+//   ];
+  
+//   function getLearnerData(course, ag, submissions) {
+//     // here, we would process this data to achieve the desired result.
+//     const result = [
+//       {
+//         id: 125,
+//         avg: 0.985, // (47 + 150) / (50 + 150)
+//         1: 0.94, // 47 / 50
+//         2: 1.0 // 150 / 150
+//       },
+//       {
+//         id: 132,
+//         avg: 0.82, // (39 + 125) / (50 + 150)
+//         1: 0.78, // 39 / 50
+//         2: 0.833 // late: (140 - 15) / 150
+//       }
+//     ];
+  
+//     return result;
+//   }
+  
+//   const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
+  
+//   console.log(result);
+function getLearnerData(course, ag, submissions) {
+    const result = [];
+  
+    submissions.forEach(submission => {
+      const learnerId = submission.learner_id;
+      const assignmentId = submission.assignment_id;
+      const submissionScore = submission.submission.score;
+  
+      const assignment = ag.assignments.find(a => a.id === assignmentId);
+  
+      if (assignment && isValidSubmission(submission, assignment)) {
+        let learnerResult = result.find(item => item.id === learnerId);
+  
+        if (!learnerResult) {
+          learnerResult = {
+            id: learnerId,
+            avg: 0,
+          };
+          result.push(learnerResult);
+        }
+  
+        const weightedScore = calculateWeightedScore(submissionScore, assignment);
+        updateLearnerResult(learnerResult, weightedScore, assignment);
+      }
+    });
+  
+    return result;
+  }
+  
+  function isValidSubmission(submission, assignment) {
+    const submissionTime = new Date(submission.submission.submitted_at);
+    const dueTime = new Date(assignment.due_at);
+  
+    return submissionTime <= dueTime;
+  }
+  
+  function calculateWeightedScore(submissionScore, assignment) {
+    let weightedScore = (submissionScore / assignment.points_possible);
+  
+    // Deduct 15% for late submissions
+    const submissionTime = new Date(submission.submission.submitted_at);
+    const dueTime = new Date(assignment.due_at);
+    if (submissionTime > dueTime) {
+      weightedScore -= 0.15;
+    }
+  
+    return weightedScore;
+  }
+  
+  function updateLearnerResult(learnerResult, weightedScore, assignment) {
+    learnerResult.avg += weightedScore;
+    learnerResult[assignment.id] = weightedScore;
+  }
+  
+  // Example data
+  const CourseInfo = {
     id: 451,
     name: "Introduction to JavaScript"
   };
   
-  // The provided assignment group.
   const AssignmentGroup = {
     id: 12345,
     name: "Fundamentals of JavaScript",
@@ -34,7 +188,6 @@ const CourseInfo = {
     ]
   };
   
-  // The provided learner submission data.
   const LearnerSubmissions = [
     {
       learner_id: 125,
@@ -78,26 +231,6 @@ const CourseInfo = {
     }
   ];
   
-  function getLearnerData(course, ag, submissions) {
-    // here, we would process this data to achieve the desired result.
-    const result = [
-      {
-        id: 125,
-        avg: 0.985, // (47 + 150) / (50 + 150)
-        1: 0.94, // 47 / 50
-        2: 1.0 // 150 / 150
-      },
-      {
-        id: 132,
-        avg: 0.82, // (39 + 125) / (50 + 150)
-        1: 0.78, // 39 / 50
-        2: 0.833 // late: (140 - 15) / 150
-      }
-    ];
-  
-    return result;
-  }
-  
   const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
-  
   console.log(result);
+  
